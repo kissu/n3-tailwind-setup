0ea7e0ca98927ab294c64daf1704cf255c15583c
Konstantin BIFERT <konstantin.bifert@gmail.com>
2022-05-03 18:35:42 +0200

    done

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-red-500">
+    nice meme
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  colors: {
+    'primary': '#0070f3',
+  }
+}
